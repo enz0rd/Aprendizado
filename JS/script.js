92fd00ededeb45dbc590d5a9fbcfb098f4d70407
Enzo Rossi Daltoé <68448627+enz0rd@users.noreply.github.com>
2022-08-22 22:01:20 -0300

    Game

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,42 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
 
-var nome = "";
-var idade = 0;
-var estudante = Boolean;
-var jogadores = [];
-
-function addJogadores(nome, idade, estudante) {
-    jogadores.push({
-        nome,
-        idade,
-        estudante
-    });
+let player1 = {
+    x: 0,
+    y: 0,
 }
 
-function consultarJog() {
-    // for(let i = 0; i < jogadores.length; i++) {
-    //     console.log(`jogadores,  
-    //     nome: ${jogadores[i].nome}
-    //     idade: ${jogadores[i].idade}
-    //     estudante: ${jogadores[i].estudante}`
-    // );
-    // }
-    jogadores.map(jogador => 
-        console.log(`jogadores
-        nome: ${jogador.nome}
-        idade: ${jogador.idade}
-        estudante: ${jogador.estudante}`
-        )
-)
-    
+function drawPlayer() {
+    context.fillStyle = "turquoise";
+    context.fillRect(player1.x, player1.y, 1, 1);
 }
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawGame() {
+    clearCanvas();
+    drawPlayer();
+}
+
+document.addEventListener('keydown', movePlayer);
+
+function movePlayer(event) {
+    if(event.key == 'ArrowUp') {
+        player1.y = player1.y - 1;
+    }
+    if(event.key == 'ArrowDown') {
+        player1.y = player1.y + 1;
+    }
+    if(event.key == 'ArrowLeft') {
+        player1.x = player1.x - 1;
+    }
+    if(event.key == 'ArrowRight') {
+        player1.x = player1.x + 1;
+    }
+    drawGame();
+}
+
+document.getElementById("clearCanvas").addEventListener("click", clearCanvas);
+document.getElementById("drawPlayer").addEventListener("click", drawPlayer);
