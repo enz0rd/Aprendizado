d1fef0eef40885641451c9fda11e9b872a7f630c
Enzo Rossi Daltoé <68448627+enz0rd@users.noreply.github.com>
2022-08-24 21:50:53 -0300

    Last commit 24/08

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,10 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-context.shadowColor='rgba(0,0,0,0)';
 
 let player1 = {
     x: 0,
     y: 0,
-    pts: 0
+    pts: 0,
 };
 
 let enemies = [{
@@ -62,24 +61,36 @@ document.addEventListener('keydown', movePlayer);
 
 function movePlayer(event) {
     if(event.key == 'ArrowUp') {
-        if(player1.y > 0) {
+        if(player1.y >= 0) {
             player1.y = player1.y - 1;
         }
     }
     if(event.key == 'ArrowDown') {
-        if(player1.y < 19) {
+        if(player1.y <= 19) {
             player1.y = player1.y + 1;
         }
     }
     if(event.key == 'ArrowLeft') {
-        if(player1.x > 0) {
+        if(player1.x >= 0) {
             player1.x = player1.x - 1;
         }
     }
     if(event.key == 'ArrowRight') {
-        if(player1.x < 19) {
+        if(player1.x <= 19) {
             player1.x = player1.x + 1;
         }
+    }
+    if(player1.y > 19) {
+        player1.y = 0;
+    }
+    if(player1.y < 0) {
+        player1.y = 19;
+    }
+    if(player1.x < 0) {
+        player1.x = 19;
+    }
+    if(player1.x > 19) {
+        player1.x = 0;
     }
     drawGame();
 }
