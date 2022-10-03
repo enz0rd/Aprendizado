33881ee499f968bc82e77b057fb27fe0f57895a1
enz0rd <enzorossidaltoe@hotmail.com.br>
2022-10-03 19:39:05 -0300

    Last adjustment

--- QA/QA02/index.js
+++ QA/QA02/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function jurosimples(dinheiroMes, taxa, tempo) {
     let jurosimples = 0
     jurosimples = dinheiroMes * taxa * tempo
@@ -33,40 +32,4 @@ function calculoIR(salarioBruto) {
     }
 }
 
-=======
-function jurosimples(dinheiroMes, taxa, tempo) {
-    let jurosimples = 0
-    jurosimples = dinheiroMes * taxa * tempo
-    return dinheiroMes + jurosimples
-}
-
-function jurocomposto(dinheiroMes, taxa, tempo) {
-    let jurocomposto = 0
-    let resultado = 0
-    jurocomposto = dinheiroMes * (Math.pow((1 + taxa), tempo))
-    resultado = jurocomposto.toFixed(2)
-    return parseFloat(resultado)
-}
-
-function calculoIR(salarioBruto) {
-    let resultado = 0
-    if(salarioBruto <= 900) {
-        resultado = 'Isento'
-        return resultado
-    } else if(salarioBruto > 900 && salarioBruto <= 1500) {
-        resultado = parseFloat(salarioBruto * 0.075).toFixed(2)
-        return parseFloat(resultado)
-    } else if(salarioBruto > 1500 && salarioBruto <= 2400) {
-        resultado = parseFloat(salarioBruto * 0.09).toFixed(2)
-        return parseFloat(resultado)
-    } else if(salarioBruto > 2400 && salarioBruto <= 3500) {
-        resultado = parseFloat(salarioBruto * 0.11).toFixed(2)
-        return parseFloat(resultado)
-    } else {
-        resultado = parseFloat(salarioBruto * 0.14).toFixed(2)
-        return parseFloat(resultado)
-    }
-}
-
->>>>>>> c7caf20c90caac02f504ff67e06ba9e44c40e412
 module.exports = {jurosimples, jurocomposto, calculoIR}
--- QA/QA02/index.test.js
+++ QA/QA02/index.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const test = require('tape')
 const index = require('./index.js')
 
@@ -42,48 +41,3 @@ test('Teste 9 - Cálculo IR', (t) => {
     t.assert(index.calculoIR(2678) === 294.58, "Resultado Correto")
     t.end()
 })
-=======
-const test = require('tape')
-const index = require('./index.js')
-
-test('Teste 1 - Juro Simples', (t) => {
-    t.assert(index.jurosimples(200, 0.05, 5) === 250, "Cálculo certo")
-    t.end()
-})
-test('Teste 2 - Juro Simples', (t) => {
-    t.assert(index.jurosimples(500, 0.1, 10) === 550, "Cálculo Errado")
-    t.end()
-})
-test('Teste 3 - Juro Simples', (t) => {
-    t.assert(index.jurosimples(500, 0.1, 10) === 1000, "Cálculo certo")
-    t.end()
-})
-
-
-test('Teste 4 - Juro Composto', (t) => {
-    t.assert(index.jurocomposto(200, 0.05, 5) === 255.26, "Cálculo certo")
-    t.end()
-})
-test('Teste 5 - Juro Composto', (t) => {
-    t.assert(index.jurocomposto(50, 0.15, 10) === 291, "Cálculo Errado")
-    t.end()
-})
-test('Teste 6 - Juro Composto', (t) => {
-    t.assert(index.jurocomposto(20, 0.1, 7) === 38.97, "Cálculo certo")
-    t.end()
-})
-
-
-test('Teste 7 - Cálculo IR', (t) => {
-    t.assert(index.calculoIR(837) === 'Isento', "Resultado Correto")
-    t.end()
-})
-test('Teste 8 - Cálculo IR', (t) => {
-    t.assert(index.calculoIR(4000) === 294.58, "Resultado Correto")
-    t.end()
-})
-test('Teste 9 - Cálculo IR', (t) => {
-    t.assert(index.calculoIR(2678) === 294.58, "Resultado Correto")
-    t.end()
-})
->>>>>>> c7caf20c90caac02f504ff67e06ba9e44c40e412
