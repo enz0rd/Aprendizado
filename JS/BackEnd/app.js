b88253b62fa799cd524c6c5c06acca2026d17aad
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2022-10-19 21:54:52 -0300

    New index.html on backend

--- JS/BackEnd/app.js
+++ JS/BackEnd/app.js
@@ -2,42 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
+const { uuid } = require('uuidv4');
 
 const app = express()
 
 app.use(cors());
 app.use(bodyParser.json())
 
-const alunos = [{
-    id: "85a8b64d-800c-415e-bbd5-30ad0e1b0dfe",
-    name: "Jorge",
-    age: 22
-},
-{
-    id: "53c0b0be-8416-4c01-9fd6-d5247cb8c2de",
-    name: "Jorge",
-    age: 33
-},
-{
-    id: "c5c8e3c4-01be-43fc-ad52-a0b27814c4ac",
-    name: "Josué",
-    age: 45
-},
-{
-    id: "d4c55086-164c-4874-8773-1f97cb4b73af",
-    name: "jhonatan",
-    age: 22
-},
-{
-    id: "c1687429-d36e-4c6a-8ede-401e9ac46c30",
-    name: "Adilson",
-    age: 22
-},
-{
-    id: "8eead2d9-39f8-422d-a991-23ce2fb1a90b",
-    name: "Mosca",
-    age: 22
-}]
+const alunos = [];
 
 app.use(express.json())
 
@@ -64,10 +36,8 @@ app.get('/alunos/comparar/:age', (req,res) => {
 })
 
 app.post('/alunos/add', (req, res) => {
-    let id = uuidv4()
-    let age = req.body.age
-    let name = req.body.name
-    alunos.push({id: id, name: name, age: age})
+    const aluno = req.body
+    alunos.push(aluno)
     res.send('Aluno adicionado!')
 })
 
