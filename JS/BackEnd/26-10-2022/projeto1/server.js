12a6b1661f194d44f20b1a1015cc4398cfb16923
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2022-11-03 21:53:46 -0300

    Node.js database implementations

--- JS/BackEnd/26-10-2022/projeto1/server.js
+++ JS/BackEnd/26-10-2022/projeto1/server.js
@@ -16,6 +16,7 @@ async function connectBanco() {
 async function start() {
     conexao = await connectBanco()
     await conexao.exec("Create table if not exists produtos (id INTEGER PRIMARY KEY, nome TEXT, preco FLOAT, loja TEXT)")
+    await conexao.exec("Create table if not exists clientes (id INTEGER PRIMARY KEY, nome TEXT, telefone VARCHAR(11), idade INTEGER, endereco VARCHAR(100), cidade VARCHAR(100))")
 
     app.listen(3000, () => {
         console.log("App Running on")
@@ -24,6 +25,12 @@ async function start() {
 }
 
 start();
+
+app.get("/produtos", async (req, res) => {
+    const produtos = await conexao.all('select * from produtos')
+
+    res.send(produtos)
+})
 
 app.post('/produtos/add', async (req, res) => {
     await conexao.run("insert into produtos (nome, preco, loja) values (:nome, :preco, :loja)",
@@ -36,6 +43,77 @@ app.post('/produtos/add', async (req, res) => {
     res.send("Produto Cadastrado!")
 })
 
+app.patch('/produtos/update/:id', async (req, res) => {
+
+    let id = req.params.id
+    let nome = req.body.nome
+    let preco = req.body.preco
+    let loja = req.body.loja
+
+    if(nome != null) {
+        if(preco != null) {
+            if(loja != null) {
+                await conexao.run(`update produtos set nome = :nome, preco = :preco, loja = :loja where id = ${id}`,
+                {
+                    ':nome': nome,
+                    ':preco': preco,
+                    ':loja': loja
+                })
+                res.send('Nome, preço e loja alterados')
+            } else if (loja == null) {
+                await conexao.run(`update produtos set nome = :nome, preco = :preco where id = ${id}`,
+                {
+                    ':nome': nome,
+                    ':preco': preco
+                })
+                res.send('Nome e preço alterados')
+            }
+        } else if(preco == null) {
+            if(loja != null) {
+                await conexao.run(`update produtos set nome = :nome, loja = :loja where id = ${id}`,
+                {
+                    ':nome': nome,
+                    ':loja': loja
+                })
+                res.send('Nome e loja alterados')
+            } else if (loja == null) {
+                await conexao.run(`update produtos set nome = :nome where id = ${id}`,
+                {
+                    ':nome': nome
+                })
+                res.send('Nome alterado')
+            }
+        }
+    } else if(nome == null) {
+        if(preco != null) {
+            if(loja != null) {
+                await conexao.run(`update produtos set preco = :preco, loja = :loja where id = ${id}`,
+                {
+                    ':preco': preco,
+                    ':loja': loja
+                })
+                res.send('Loja e Preço alterados')
+            } else if (loja == null) {
+                await conexao.run(`update produtos set preco = :preco where id = ${id}`,
+                {
+                    ':preco': preco
+                })
+                res.send('Preço alterado')
+            }
+        } else if(preco == null) {
+            if(loja != null) {
+                await conexao.run(`update produtos set loja = :loja where id = ${id}`,
+                {
+                    ':loja': loja
+                })
+                res.send('Loja alterada')
+            } else if (loja == null) {
+                res.send('nada alterado')
+            }
+        }
+    }
+})
+
 app.delete('/produtos/:id', async (req, res) => {
     await conexao.run('delete from produtos where id = :id',
     {
@@ -45,8 +123,27 @@ app.delete('/produtos/:id', async (req, res) => {
     res.send("Produto Deletado!")
 })
 
-app.get("/produtos", async (req, res) => {
-    const produtos = await conexao.all('select * from produtos')
+app.get("/clientes", async (req, res) => {
+    const clientes = await conexao.all('select * from clientes')
 
-    res.send(produtos)
+    res.send(clientes)
+})
+
+app.get("/clientes/:id", async (req, res) => {
+    const clientes = await conexao.all(`select * from clientes where id = ${req.params.id}`)
+
+    res.send(clientes)
+})
+
+app.post('/clientes/add', async (req, res) => {
+    await conexao.run("insert into clientes (nome, telefone, idade, endereco, cidade) values (:nome, :telefone, :idade, :endereco, :cidade)",
+    {
+        ':nome': req.body.nome,
+        ':telefone': req.body.telefone,
+        ':idade': req.body.idade,
+        ':endereco': req.body.endereco,
+        ':cidade': req.body.cidade
+    })
+
+    res.send("Cliente Cadastrado!")
 })
--- JS/BackEnd/26-10-2022/projeto2/server.js
+++ JS/BackEnd/26-10-2022/projeto2/server.js
@@ -1,27 +1,145 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser')
-const sqlite3 = require('sqlite3')
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes, STRING, INTEGER} = require('sequelize');
+const app = express();
+
+app.use(bodyParser.json())
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './database.db'
+});
+
+const Produto = sequelize.define('produto', {
+    nome: {
+      type: DataTypes.STRING(50),
+      allowNull: false,         
+    },
+    preco: {
+      type: DataTypes.FLOAT
+    },
+    loja: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    validade: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+  }, {
+    timestamps: true,
+    paranoid: true
+});
+
+app.post('/produtos', async (req, res) => {
+    const produto = await Produto.create({
+        nome: req.body.nome,
+        preco: req.body.preco,
+        loja: req.body.loja,
+        validade: req.body.validade
+    })
+    res.json(produto);
+});
+
+app.delete('/produtos/:id',async( req, res ) => {
+    await Produto.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.send('deletado')
 })
 
-const app = express()
+app.get('/produtos', async (req,res) => {
+    const produtos = await Produto.findAll();
+    res.json(produtos);
+})
 
-app.use(bodyParser.json())
+app.put('/produtos/:id', async (req, res) => {
+    const produto = await Produto.findByPk(req.params.id);
+    produto.nome = req.body.nome;
+    produto.preco = req.body.preco;
+    produto.loja = req.body.loja;
+    produto.validade = req.body.validade;
+    produto.save();
+    res.json(produto);
+})
+
+const Cliente = sequelize.define('clientes', {
+    nome: {
+        type: STRING(50),
+        allowNull: false
+    },
+    idade: {
+        type: INTEGER,
+        allowNull: false
+    },
+    telefone: {
+        type: INTEGER
+    },
+    endereco: {
+        type: STRING(50)
+    },
+    cidade: {
+        type: STRING(20)
+    },
+    uf: {
+        type: STRING(2)
+    }
+}, {
+    timestamps: true,
+    paranoid: true
+})
+
+app.post('/clientes', async (req, res) => {
+    const cliente = await Cliente.create({
+        nome: req.body.nome,
+        idade: req.body.idade,
+        telefone: req.body.telefone,
+        endereco: req.body.endereco,
+        cidade: req.body.cidade,
+        uf: req.body.uf
+    })
+    res.json(cliente);
+});
+
+app.delete('/clientes/:id',async( req, res ) => {
+    await Cliente.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.send('deletado')
+})
+
+app.get('/clientes', async (req,res) => {
+    const clientes = await Cliente.findAll();
+    res.json(clientes);
+})
+
+app.put('/clientes/:id', async (req, res) => {
+    const clientes = await Cliente.findByPk(req.params.id);
+    clientes.nome = req.body.nome;
+    clientes.idade = req.body.idade;
+    clientes.telefone = req.body.telefone;
+    clientes.endereco = req.body.endereco;
+    clientes.cidade = req.body.cidade;
+    clientes.uf = req.body.uf;
+    clientes.save();
+    res.json(clientes);
+})
 
 async function start() {
     try {
         await sequelize.authenticate();
-        console.log("connection has been established successfully.");
+        console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('unable to connect to the database: ', error);
+        console.error('Unable to connect to the database:', error);
     }
-
+    sequelize.sync();
     app.listen(3000, () => {
         console.log('app is running')
     })
 }
-start ()
+
+start();
