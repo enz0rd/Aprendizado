f623133faf76848f8ca88bb11381fa73f9d2c7c4
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2022-11-07 20:48:14 -0300

    New twitter project

---
+++ JS/BackEnd/07-11-22/server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const sqlite = require('sqlite')
+const cors = require('cors')
+const sqlite3 = require('sqlite3')
+const bodyParser = require('body-parser')
+const { Sequelize, DataTypes } = require('sequelize')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.db'
+});
+
+const User = sequelize.define('users', {
+    name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    }
+}, {
+    timestamps: true,
+    paranoid: true
+})
+
+const Post = sequelize.define('post', {
+    message: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+User.hasMany(Post)
+
+app.get('/', (req,res) => {
+    res.send('Página Inicial')
+})
+
+app.post('/', (req,res) => {
+    res.send(`<script>
+    alert('Success')
+    
+    setTimeout(function () {
+        window.location.href = "/"
+    }, 1000)
+
+    </script>`)
+})
+
+app.get('/users', async (req,res) => {
+    const users = await User.findAll();
+    res.json(users)
+})
+
+app.post('/users', async (req,res) => {
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    res.json(user)
+})
+
+async function start() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+    sequelize.sync();
+    app.listen(3000, () => {
+        console.log('app is running')
+    })
+}
+
+start();
